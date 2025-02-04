30824af0dfa5c619ecf568a3962304b17f5c8661
Lijiao Ning <lijiao.ning91@gmail.com>
2025-02-04 16:18:28 +0100

    update exos script #6

--- ioc_r/week05/r_w05_exos.R
+++ ioc_r/week05/r_w05_exos.R
@@ -1,22 +1,20 @@
 ### R script for the hands-on examples
-### Week 4
+### Week 5
 
 ## Install New Packages -------------------------------------------------------------
 
-# We will use a couple of additional R packages for this training in sessions 8 and 9:
+# We will use a couple of additional R packages for this training
+# in sessions 8 and 9, for example:
 # - {[tidyr](https://tidyr.tidyverse.org)}: provides functions that help you get to tidy data
-# - {[dplyr](https://dplyr.tidyverse.org)}: for data manipulation
-# - {[tibble](https://tibble.tidyverse.org/)}: a modern re-imagining of the data frame
-# - {[stringr](https://stringr.tidyverse.org/)}: provides functions designed to make working with strings as easy as possible
 # - {[readr](https://readr.tidyverse.org)}: provides fast and friendly way to read rectangular data 
 
-# Instead of installing them one by one, we can install the {[tidyverse](https://www.tidyverse.org)} package (a collection of previous packages designed for data science).
 
-## 1. Install the {`tidyverse`} package.
-install.packages("tidyverse") # you only need to install it once
+## 1.Install the {`readr`} and {`tidyr`} packages.
+# install.packages("readr") # you only need to install it once
+# install.packages("tidyr") # you only need to install it once
 
 
-## 2. Check if you can find `{dplyr}` and `{tidyr}` 
+## 2. Check if you can find `{readr}` and `{tidyr}` 
 ##    in the "Packages" panel in RStudio.
 
 ## 3. Please use the following code to create a data frame of
@@ -28,60 +26,84 @@ my_pkgs <- as.data.frame(installed.packages()[, c(1, 3:4)])
 ## 4. According to `my_pkgs`, how many packages are installed in your system?
 
 
-## 5. Extract the rows for the "ggplot2", "tidyr", "dplyr" packages from `my_pkgs`.
+## 5. Extract the rows for the "ggplot2", "tidyr", "readr" packages from `my_pkgs`.
 
 
 
-## Create a Basic ggplot -------------------------------------------------------------
+## Create a Basic Histogram -------------------------------------------------------------
 
-# We'll use the built-in data set `iris`.
+# In the hands-on examples of session 1 and 2,
+# we have drawn histograms with the basic R function `hist()`.
 
-## 0. Load the {`ggplot2`} package.
+# Now let's try with functions from {`ggplot2`} package.
 
-## 1. Create a violin for the petal length by species.
+## 0. Import the `read-counts.csv` file into RStudio and name the data as `counts`.
+
+## 1. Load the {`ggplot2`} package.
+##    Then create a histogram with the function `geom_histogram()`
+##      for all genes of the sample "WT.2".
+##    Have you noticed the message from `geom_histogram()` about the bins?
+
+
+## 2.Create a new histogram for the sample "WT.2",
+##    but this time use the log2 of the gene counts.
+##   Do you see the messages from `geom_histogram()`?
+
+
+
+## 3. Try 10 and 20 for the `bins` parameter of `geom_histogram()`.
+##    Observe how the histogram changes.
+
+
+## 4. We will use 10 bins for the histogram.
+##    Change the color of the bars to blue.
+
+
+## 5. Based on the previous figure, fill the bars in orange.
+
+## 6. Based on the previous figure, change:
+##    - x-axis title to be: "log2 gene expression"
+##    - y-axis title to be: "Counts"
+##    - plot title to show sample name and the number of genes used.
+
+## 7. Based on the previous figure, use the theme `theme_bw()`.
+
+
+## Create a Boxplot with ggplot --------------------------------------------
+
+## In the hands-on examples of session 3,
+##  we demonstrated that there is a significant difference in
+##  the expression of *LOH1* between WT and SET1 samples.
+
+## This gene is upregulated in SET1 samples compared to WT samples,
+##  the log2 fold change is 2.85.
+
+## Now, let's draw a more sophisticated boxplot with ggplot2.
+
+## 0. Prepare a data frame for the boxplot. We need:
+##    - a column for the expression levels of different samples
+##    - a column to indicate what is the sample group (WT or SET1)
+
+
+## 1. Create a boxplot for the expression level by sample group.
 
 
 ## 2. Modify labels:
 ##    - Remove x-axis title
-##    - Change y-axis title to "Petal Length (cm)"
-##    - Add a plot title "Distribution of iris' petal length"
-##    - Add a subtitle "The data were collected by Anderson, Edgar (1935)"
+##    - Change y-axis title to "Expression Level"
+##    - Add a plot title "Expression of LOH1 in SET1 and WT samples"
+##    - Add a subtitle "log2FoldChange = 2.85"
 
 
+## 3. Add a layer of scatter plot over the boxplot.
+##    - Color the points by sample group.
+##    - Use the `alpha` parameter to let points be semi-transparent.
 
-## 3. Add a layer of boxplot on the violin plot.
-##    - Color the boxes by species.
-##    - Use the `alpha` parameter to let boxes be semi-transparent.
 
-
-## 4. Change the theme to `theme_minimal()`. 
+## 4. Change the theme to `theme_minimal()`.
 
 
 ## 5. With the `theme()` function:
-##    - Increase the font size of x-axis' text.
-##    - Move the legend to the top of the plot.
-
-
-## Create a Volcanot Plot with ggplot2 --------------------------------------------
-
-# We will use the same diffenrential expression (DE) analysis result as in the hands-on example of session 4, which is also available [here](https://raw.githubusercontent.com/InforBio/IOC/refs/heads/main/ioc_r/exos_data/toy_DEanalysis.csv).
-
-## 1. Import the results file in R and name it `de_res`.
-
-
-## 2. Create a new column (`gene_signif`) which indicates
-##    whether the adjusted p-value is smaller than 0.05.
-
-
-## 3. Create a volcano plot using the DE results `de_res`.
-##    - Let the log2 fold change be on the x-axis.
-##    - Transform the adjusted p-value to `-log10()`,
-##      then let them be on the y-axis.
-##    - Color the points by whether the adjusted p-value is smaller than 0.05
-##      (*i.e.*, the `gene_signif` column).
-##    - Use the shape 18 for the points.
-##    - Increase the points size to 3.
-##    - Use the `alpha` parameter to make points transparent.
-##    - Add a title for the plot, show number of analysed genes in the subtitle.
-##    - Use the `theme_bw()` for the plot.
+##    - Move the plot title to the center.
 ##    - Hide the legend.
+
